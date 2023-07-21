3bd5c1e1d913ea93fb31303709a078bec7270f2b
Ryunosuke_Ozaki <138109860+ryunosuke1990127@users.noreply.github.com>
2023-07-21 11:52:12 +0000

    update3

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
   # サイトのルートページを作成
   root to: 'homes#top'
   devise_for :users
-    
+
     # post_image_controller用のルーティング
    resources :post_images, only: [:new, :index, :show, :create,:destroy]
-   
+
     # users_controller用のルーティング
-    resources :users, only: [:show, :edit]
-   
+    resources :users, only: [:show, :edit, :update]
+
    get '/homes/about' =>'homes#about', as:'about'
   end
