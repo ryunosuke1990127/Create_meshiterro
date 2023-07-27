04eebb262553e230b23c641ec6647f378038e015
Ryunosuke_Ozaki <138109860+ryunosuke1990127@users.noreply.github.com>
2023-07-27 07:42:22 +0000

    update final

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
+  # クリックした情報のIDと今ログインしているユーザーが正しいかチェックを行う
+   before_action :is_matching_login_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
-    end
+  end
 
   def edit
     @user = User.find(params[:id])
@@ -12,13 +14,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
-
   end
 
-
   private
-
   def user_params
     params.require(:user).permit(:name,:profile_image)
   end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+  unless user.id == current_user.id
+    redirect_to post_images_path
+  end
+  end
+
 end
